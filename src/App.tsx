54d263ebc4b356585212e9a1afc990aee50c3585
Volcomix <Volcomix@users.noreply.github.com>
2021-05-02 14:29:24 +0200

    Get account information

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import { useEffect, useState } from 'react'
 import theme from './theme'
+
+const encoder = new TextEncoder()
+
+function toHex(data: ArrayBuffer): string {
+  return Array.from(new Uint8Array(data))
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('')
+}
 
 export default function App() {
   const classes = useStyles()
@@ -13,6 +22,28 @@ export default function App() {
     setApiKey(localStorage.getItem('apiKey') ?? '')
     setSecretKey(localStorage.getItem('secretKey') ?? '')
   }, [])
+
+  async function getAccountInformation() {
+    const key = await crypto.subtle.importKey(
+      'raw',
+      encoder.encode(secretKey),
+      { name: 'HMAC', hash: 'SHA-256' },
+      true,
+      ['sign']
+    )
+    const timestamp = Date.now()
+    const queryString = `timestamp=${timestamp}`
+    const signature = await crypto.subtle.sign(
+      'HMAC',
+      key,
+      encoder.encode(queryString)
+    )
+    const response = await fetch(
+      `/api/v3/account?${queryString}&signature=${toHex(signature)}`,
+      { headers: { 'X-MBX-APIKEY': apiKey } }
+    )
+    console.log(await response.json())
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -38,6 +69,12 @@ export default function App() {
             localStorage.setItem('secretKey', event.target.value)
           }}
         />
+        <Button
+          disabled={!(apiKey && secretKey)}
+          onClick={getAccountInformation}
+        >
+          Get account information
+        </Button>
       </div>
     </ThemeProvider>
   )
